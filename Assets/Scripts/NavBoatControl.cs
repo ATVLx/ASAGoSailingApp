d3834123e98050d6f79f064006b47d5f4184ef16
Eric Espinoza <ericespinoza@outlook.com>
2015-12-03 14:51:31 -0800

    Rudder and boom rotation slowed down. Increased rudder null zone.

--- Assets/Scripts/NavBoatControl.cs
+++ Assets/Scripts/NavBoatControl.cs
@@ -17,13 +17,13 @@ public class NavBoatControl : MonoBehaviour {
 	/// <summary>
 	/// The rudder rotation speed in degrees/sec.
 	/// </summary>
-	private float rudderRotationSpeed = 100f;
+	private float rudderRotationSpeed = 60f;
 	/// <summary>
 	/// The boom trim speed in degrees/sec.
 	/// </summary>
-	private float boomTrimSpeed = 50f;
+	private float boomTrimSpeed = 30f;
 	private float maxRudderRotation = 60f;
-	private float rudderNullZone = 0.1f;
+	private float rudderNullZone = 0.15f;
 	private float boatRotationVelocityScalar = 1f;
 	private float boatMovementVelocityScalar = 100f;
 	private Quaternion comeAboutStart, comeAboutEnd;
