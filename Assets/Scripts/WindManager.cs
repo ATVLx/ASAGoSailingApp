14ca69819e93e3ac5e2f5a055fd39b277b19fe0a
Pablo Leon-Luna <paleonluna@gmail.com>
2015-12-13 11:00:33 -0800

    randomized wind arrows more

--- Assets/Scripts/WindManager.cs
+++ Assets/Scripts/WindManager.cs
@@ -28,7 +28,8 @@ public class WindManager : MonoBehaviour {
 			Vector3 tempPosition = GameObject.FindGameObjectWithTag ("Player").transform.position;
 			transform.position = new Vector3 (tempPosition.x, 2f, tempPosition.z + distFromBoatToSpawn);
 			int rand = Random.Range (0, spawnPositions.Length-1);
-			Instantiate (arrowPrefab, spawnPositions [rand].position, Quaternion.Euler(new Vector3(0,180,0)));
+			float xDisplace = Random.Range (-4f, 4f);
+			Instantiate (arrowPrefab, new Vector3(spawnPositions [rand].position.x + xDisplace, spawnPositions [rand].position.y, spawnPositions [rand].position.z), Quaternion.Euler(new Vector3(0,180,0)));
 		}
 	}
 }
