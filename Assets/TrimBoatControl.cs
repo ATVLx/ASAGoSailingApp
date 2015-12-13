57046c919f4fdd6f2d48290e7cd486af0266c356
Pablo Leon-Luna <paleonluna@gmail.com>
2015-12-13 07:38:56 -0800

    fixed the steering issue

--- Assets/TrimBoatControl.cs
+++ Assets/TrimBoatControl.cs
@@ -14,6 +14,7 @@ public class TrimBoatControl : NavBoatControl {
 	// Update is called once per frame
 	void Update () {
 		MastRotation ();
+		IdentifyPointOfSail ();
 		sailEffiencySlider.value = sailEffectiveness;
 		print (sailEffectiveness);
 	}
