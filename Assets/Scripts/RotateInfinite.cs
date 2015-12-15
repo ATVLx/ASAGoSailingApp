c8d66b550bdf7635fc00f3fd6ecdde766dd75548
paleonluna <paleonluna@gmail.com>
2015-12-14 18:38:38 -0800

    main menu scene and prefabs

---
+++ Assets/Scripts/RotateInfinite.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateInfinite : MonoBehaviour {
+
+	public float xAxis, yAxis, zAxis;
+
+	void Update () {
+		transform.Rotate (xAxis, yAxis, zAxis);
+	}
+}
