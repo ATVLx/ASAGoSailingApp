eedf8fbcf3f5202452d7eba4c08b59181432a7f5
paleonluna <paleonluna@gmail.com>
2015-12-15 15:20:46 -0800

    added in DELEGATES! :D

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public void PointsOfSailButton () {
+		print ("POINTS OF SAIL CLICKED");
+		ConfirmationPopUp.s_instance.InitializeConfirmationPanel( (bool answer) => {
+			if (answer) {
+				print("YES!!!!!");
+			}
+			else {
+				print("NOOOOOO");
+			}
+		});
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/ConfirmationPopUp.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ConfirmationPopUp : MonoBehaviour {
+
+	[SerializeField]
+	GameObject confimationPanel;
+
+	public static ConfirmationPopUp s_instance;
+
+	void Awake () {
+		if (s_instance == null) {
+			s_instance = this;
+		} else {
+			Destroy (this.gameObject);
+			Debug.LogWarning("Deleted " + gameObject.name + " because it was a duplicate confirmation pop up instance");
+		}
+	}
+
+	public delegate void ConfirmationFunction (bool confirmationBool);
+	public ConfirmationFunction myConfirmationDelegate = null;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void InitializeConfirmationPanel (ConfirmationFunction thisFunction) {
+		confimationPanel.SetActive (true);
+		myConfirmationDelegate = thisFunction;
+	}
+
+	public void Confirm () {
+		myConfirmationDelegate (true);
+		myConfirmationDelegate = null;
+		confimationPanel.SetActive (false);
+
+	}
+
+	public void Deny () {
+		myConfirmationDelegate (false);
+		myConfirmationDelegate = null;
+		confimationPanel.SetActive (false);
+
+	}
+}
