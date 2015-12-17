5fcfa9187e26e0908105cd2df19525b72ba9371f
Eric Espinoza <ericespinoza@outlook.com>
2015-12-17 13:58:35 -0800

    Updated GhostPathRecorder so that it uses only "Y" rotations instead of entire Quaternions.

--- Assets/Scripts/GhostMatch/GhostPathRecorder.cs
+++ Assets/Scripts/GhostMatch/GhostPathRecorder.cs
@@ -11,7 +11,7 @@ public class GhostPathRecorder : MonoBehaviour {
 	public bool fetch = false;
 
 	public List<Vector3> recordedPositions;
-	public List<Quaternion> recordedRotations;
+	public List<float> recordedYRotations;
 	private Transform thisTransform;
 	private float timer;
 	private Rester _Rester;
@@ -19,7 +19,7 @@ public class GhostPathRecorder : MonoBehaviour {
 
 	void Start () {
 		recordedPositions = new List<Vector3>();
-		recordedRotations = new List<Quaternion>();
+		recordedYRotations = new List<float>();
 		thisTransform = GetComponent<Transform>();
 
 		_Rester = GameObject.FindObjectOfType<Rester>();
@@ -39,7 +39,7 @@ public class GhostPathRecorder : MonoBehaviour {
 
 		if( exportJSON ) {
 			exportJSON = false;
-			if( recordedPositions.Count < 2 || recordedRotations.Count < 2 ) {
+			if( recordedPositions.Count < 2 || recordedYRotations.Count < 2 ) {
 				Debug.LogError( "GhostPathRecorder hasn't recorded anything yet so there is nothing to export." );
 			} else {
 				ExportRecordedDataToJSON();
@@ -50,7 +50,7 @@ public class GhostPathRecorder : MonoBehaviour {
 			if( timer >= sampleRate ) {
 				// Record position and rotation
 				recordedPositions.Add( thisTransform.position );
-				recordedRotations.Add( thisTransform.rotation );
+				recordedYRotations.Add( thisTransform.rotation.y );
 
 				// Add the left over faction of time to the timer after reset
 				timer = timer % sampleRate;
@@ -71,23 +71,24 @@ public class GhostPathRecorder : MonoBehaviour {
 		for( int i = 0; i < recordedPositions.Count; i++ ) {
 			// Get reference to current position/rotation in our recorded data
 			Vector3 currentPos = recordedPositions[i];
-			Quaternion currentRot = recordedRotations[i];
+			float currentYRot = recordedYRotations[i];
 
 			// Build out position and rotation objects
 			JsonObject posV = new JsonObject();
 			posV.Add( "x", currentPos.x );
 			posV.Add( "y", currentPos.y );
 			posV.Add( "z", currentPos.z );
-			JsonObject rotQ = new JsonObject();
-			rotQ.Add( "w", currentRot.w );
-			rotQ.Add( "x", currentRot.x );
-			rotQ.Add( "y", currentRot.y );
-			rotQ.Add( "z", currentRot.z );
+			
+//			JsonObject rotQ = new JsonObject();
+//			rotQ.Add( "w", currentYRot.w );
+//			rotQ.Add( "x", currentYRot.x );
+//			rotQ.Add( "y", currentYRot.y );
+//			rotQ.Add( "z", currentYRot.z );
 
 			// Create and populate object that will encapsulate position and rotation at this index
 			JsonObject posRotPair = new JsonObject();
 			posRotPair.Add( "posV", posV );
-			posRotPair.Add( "rotQ", rotQ );
+			posRotPair.Add( "yRot", currentYRot );
 
 			// Add new object to array of positions
 			posRotArray.Add( posRotPair );
@@ -104,7 +105,7 @@ public class GhostPathRecorder : MonoBehaviour {
 	public void StartRecording() {
 		isRecording = true;
 		recordedPositions.Add( thisTransform.position );
-		recordedRotations.Add( thisTransform.rotation );
+		recordedYRotations.Add( thisTransform.rotation.y );
 	}
 
 	public void StopRecording() {
