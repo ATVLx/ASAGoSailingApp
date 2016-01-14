106f4ed8aa4e1e501e070d47c43eae12dfc1e665
Eric Espinoza <ericespinoza@outlook.com>
2016-01-14 14:52:42 -0800

    Added World Reset Trigger to Apparent Wind module. Moved done button.

---
+++ Assets/Scripts/Misc/WorldResetTrigger.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Placed on an enormous sphere collider and serves as the bounds for the world. If the player exits the trigger, his position is set back to the center of the level.
+/// </summary>
+public class WorldResetTrigger : MonoBehaviour {
+	void OnTriggerExit( Collider col ) {
+		if( col.tag == "Player" ) {
+			Vector3 resetPosition = new Vector3( 0f, col.transform.position.y, 0f );
+			col.transform.position = resetPosition;
+		}
+	}
+}
