89c5de87aef3b59c6dabfd80340cca52657dfaad
paleonluna <paleonluna@gmail.com>
2016-01-15 17:48:10 -0800

    progress on the MOB module - added landing zones

---
+++ Assets/EnterLandingZone.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnterLandingZone : MonoBehaviour {
+
+	void OnTriggerExit(Collider other) {
+		if (other.tag == "Player") {
+			MOBManager.s_instance.WinScenario ();
+		}
+	}
+}
