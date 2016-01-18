8bf3f9aee9b155fda767ab331bbb610dc981b96e
Eric Espinoza <ericespinoza@outlook.com>
2016-01-18 15:48:33 -0800

    Updates to ApparentWind

--- Assets/Scripts/Boat/ApparentWindBoatControl.cs
+++ Assets/Scripts/Boat/ApparentWindBoatControl.cs
@@ -2,12 +2,22 @@
 using System.Collections;
 
 public class ApparentWindBoatControl : MonoBehaviour {
- 	
+	public static ApparentWindBoatControl s_instance;
+
 	public Transform boatBody;
 	public float speedInKnots = 4f;
+	[System.NonSerialized]
+	public Rigidbody myRigidbody;
 
 	private Transform myTransform;
-	private Rigidbody myRigidbody;
+
+	void Awake() {
+		if( s_instance == null ) {
+			s_instance = this;
+		} else {
+			Debug.LogWarning( gameObject.name + " is being deleted because it is a duplicate " + this.name + ".");
+		}
+	}
 
 	void Start() {
 		myTransform = transform;
