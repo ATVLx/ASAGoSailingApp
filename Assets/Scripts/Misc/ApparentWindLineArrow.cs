a344d79b7a82fa6ef7e97ab5ce33a82ea12cb1b5
Eric Espinoza <ericespinoza@outlook.com>
2016-01-19 12:46:08 -0800

    More work on ApparentWind

---
+++ Assets/Scripts/Misc/ApparentWindLineArrow.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Controls the arrows that follow the wind lines in the ApparentWind module.
+/// </summary>
+public class ApparentWindLineArrow : MonoBehaviour {
+
+	private Transform myTransform;
+	private Transform origin;
+	private Transform destination;
+	private float speed;
+	private float percentageTraveled = 0f;
+
+	void Start() {
+		myTransform = transform;
+	}
+	
+	public void Initialize( Transform _origin, Transform _destination, float _speed, Color color ) {
+		origin = _origin;
+		destination = _destination;
+		speed = _speed;
+		GetComponentInChildren<SpriteRenderer>().color = color;
+		
+		myTransform.LookAt( destination );
+	}
+		
+	void Update () {
+		float totalDistance = Vector3.Distance( origin.position, destination.position );
+
+		myTransform.position = Vector3.Lerp( origin.position, destination.position, percentageTraveled );
+		myTransform.LookAt( destination );
+	}
+}
---
+++ Assets/Scripts/Misc/DestroyTrigger.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Collider))]
+/// <summary>
+/// Destroys any object marked with the "Destroyable" tag. Requires a collider and marks it as a trigger.
+/// </summary>
+public class DestroyTrigger : MonoBehaviour {
+
+	void Start() {
+		GetComponent<Collider>().isTrigger = true;
+	}
+
+	void OnTriggerEnter( Collider col ) {
+		if( col.tag == "Destroyable" )
+		{
+			Destroy( col.gameObject );
+		}
+	}
+}
