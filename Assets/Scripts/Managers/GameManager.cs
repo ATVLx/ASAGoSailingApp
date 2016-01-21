bcb5b971cbd5a9114b4e12560359d7599eaa4523
Eric Espinoza <ericespinoza@outlook.com>
2016-01-20 19:50:38 -0800

    Completed Pause Menu.

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -9,17 +9,27 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour {
 	public static GameManager s_instance;
 
-	public enum LevelState {MainMenu, POS, SailTrim, ApparentWind, LearnToTack, Docking, Navigation};
+	public enum LevelState {OpeningCredits, MainMenu, POS, SailTrim, ApparentWind, LearnToTack, Docking, Navigation};
 	public LevelState thisLevelState;
 
 	// Game pausing event
-	public delegate void GamePause( bool toggle );
-	public static event GamePause PauseEvent;
+	public delegate void PauseEvent( bool toggle );
+	public static event PauseEvent TogglePause;
 
+	// Volume change events
+	public delegate void MusicVolumeChangeEvent( float value );
+	public static event MusicVolumeChangeEvent MusicVolumeChanged;
+	public delegate void SoundsVolumeChangeEvent( float value );
+	public static event SoundsVolumeChangeEvent SoundsVolumeChanged;
+
+	[System.NonSerialized]
 	public bool isPaused = false;
 
 	[Header("UI")]
 	public CanvasGroup pauseMenu;
+	public CanvasGroup optionsMenu;
+	public Slider musicVolumeSlider;
+	public Slider soundsVolumeSlider;
 
 	void Awake() {
 		if (s_instance == null) {
@@ -27,6 +37,7 @@ public class GameManager : MonoBehaviour {
 			DontDestroyOnLoad( this.gameObject );
 		}
 		else {
+			Debug.LogWarning( "Deleting " + gameObject.name + " because it is a duplicate GameManager" );
 			Destroy(gameObject);
 		}
 	}
@@ -36,34 +47,81 @@ public class GameManager : MonoBehaviour {
 		SceneManager.LoadScene (levelIndex);
 	}
 
+	#region UI Logic
 	public void PressedPause() {
-		if( PauseEvent != null ) {
-			PauseEvent( true );
+		if( TogglePause != null ) {
+			TogglePause( true );
 		}
 
 		isPaused = true;
-		pauseMenu.alpha = 1f;
-		pauseMenu.interactable = true;
-		pauseMenu.blocksRaycasts = true;
+		ToggleSlide( pauseMenu, true );
 	}
 
 	public void ResumeGame() {
 		isPaused = false;
-		pauseMenu.alpha = 0f;
-		pauseMenu.interactable = false;
-		pauseMenu.blocksRaycasts = false;
+		ToggleSlide( pauseMenu, false );
 
-		if( PauseEvent != null ) {
-			PauseEvent( false );
+		if( TogglePause != null ) {
+			TogglePause( false );
 		}
 	}
 
 	public void RestartModule() {
+		ToggleSlide( pauseMenu, false );
 		LoadLevel( (int)thisLevelState );
 	}
 
 	public void ReturnToMainMenu() {
+		ToggleSlide( pauseMenu, false );
 		LoadLevel( (int)LevelState.MainMenu );
+	}
+
+	public void PressedOptionsButton() {
+		ToggleSlide( pauseMenu, false );
+		ToggleSlide( optionsMenu, true );
+	}
+
+	/// <summary>
+	/// Returns to pause screen. Used when pressing the 'Close' button in the options menu.
+	/// </summary>
+	public void ReturnToPauseScreen() {
+		ToggleSlide( optionsMenu, false );
+		ToggleSlide( pauseMenu, true );
+	}
+
+	public void UpdatedMusicVolume() {
+		if( MusicVolumeChanged != null ) {
+			MusicVolumeChanged( musicVolumeSlider.value );
+		}
+	}
+
+	public void UpdatedSoundsVolume() {
+		if( SoundsVolumeChanged != null ) {
+			SoundsVolumeChanged( soundsVolumeSlider.value );
+		}
+	}
+
+	/// <summary>
+	/// Toggles the slide on or off.
+	/// </summary>
+	/// <param name="cG">Reference to CanvasGroup instance.</param>
+	/// <param name="turnOn">If set to <c>true</c> toggles CanvasGroup on.</param>
+	private void ToggleSlide( CanvasGroup cG, bool turnOn ) {
+		if( turnOn ) {
+			cG.alpha = 1f;
+			cG.interactable = true;
+			cG.blocksRaycasts = true;
+		} else {
+			cG.alpha = 0f;
+			cG.interactable = false;
+			cG.blocksRaycasts = false;
+		}
+	}
+	#endregion
+
+	void Update() {
+		if( Input.GetKeyDown( KeyCode.Space ) )
+			PressedPause();
 	}
 }
 
