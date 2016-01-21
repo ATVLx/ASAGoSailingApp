f5592b4eb26c7a634522385517720aff9f58c2f3
Eric Espinoza <ericespinoza@outlook.com>
2016-01-21 12:42:22 -0800

    Added logic to pause button and added one to every scene. Overloaded ConfirmationPopUp's initialize method to support titles.

--- Assets/Scripts/GUI/ConfirmationPopUp.cs
+++ Assets/Scripts/GUI/ConfirmationPopUp.cs
@@ -2,11 +2,9 @@
 using System.Collections;
 using UnityEngine.UI;
 
-/*
-	This class uses a delegate to store a function to be executed after a callback if it returns true,
-	used for "are you sure you want to quit" type of prompts
-*/
-
+/// <summary>
+/// Uses a delegate to store a function to be executed after a callback if it returns true, used for "are you sure you want to quit" type of prompts
+/// </summary>
 public class ConfirmationPopUp : MonoBehaviour {
 	public static ConfirmationPopUp s_instance;
 
@@ -14,6 +12,8 @@ public class ConfirmationPopUp : MonoBehaviour {
 	GameObject confimationPanel;
 	[SerializeField]
 	Text descriptionOfActionText;
+	[SerializeField]
+	Text popUpTitleText;
 
 	public delegate void ConfirmationFunction (bool confirmationBool);
 	public static ConfirmationFunction myConfirmationDelegate = null;
@@ -27,12 +27,38 @@ public class ConfirmationPopUp : MonoBehaviour {
 		}
 	}
 
+	/// <summary>
+	/// Initializes values and toggles on the confirmation panel.
+	/// </summary>
+	/// <param name="descriptionOfAction">Text to be shown when asking for confirmation.</param>
+	/// <param name="callBack">Call back to be executed when this method exits.</param>
 	public void InitializeConfirmationPanel (string descriptionOfAction, ConfirmationFunction callBack) {
 		confimationPanel.SetActive (true);
 		if( descriptionOfActionText != null )
 			descriptionOfActionText.text = descriptionOfAction;
 		else
 			Debug.LogError( gameObject.name +"'s ConfirmationPopUp component is missing a reference for DescriptionOfActionText.");
+		myConfirmationDelegate = callBack;
+	}
+
+	/// <summary>
+	/// Initializes values and toggles on the confirmation panel.
+	/// </summary>
+	/// <param name="popUpTitle">Title text on the confirmation panel.</param>
+	/// <param name="descriptionOfAction">Text to be shown when asking for confirmation.</param>
+	/// <param name="callBack">Call back to be executed when this method exits.</param>
+	public void InitializeConfirmationPanel (string popUpTitle, string descriptionOfAction, ConfirmationFunction callBack) {
+		confimationPanel.SetActive (true);
+		// Set popup's title
+		if( popUpTitleText != null )
+			popUpTitleText.text = popUpTitle;
+		else
+			Debug.LogError( gameObject.name +"'s ConfirmationPopUp component is missing a reference for PopUpTitleText." );
+		// Set confirmation text
+		if( descriptionOfActionText != null )
+			descriptionOfActionText.text = descriptionOfAction;
+		else
+			Debug.LogError( gameObject.name +"'s ConfirmationPopUp component is missing a reference for DescriptionOfActionText." );
 		myConfirmationDelegate = callBack;
 	}
 
@@ -46,6 +72,5 @@ public class ConfirmationPopUp : MonoBehaviour {
 		myConfirmationDelegate (false);
 		myConfirmationDelegate = null;
 		confimationPanel.SetActive (false);
-
 	}
 }
