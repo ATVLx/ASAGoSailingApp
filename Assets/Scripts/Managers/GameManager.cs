a1383e61aac1f5dce7390b6ff33b203ab291aef1
Eric Espinoza <ericespinoza@outlook.com>
2016-01-20 17:28:53 -0800

    Introduced simple pause menu into permanent canvas in Main Menu scene.

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,17 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-/*
-	This class keeps track of state the game is in wrt to levels and scenes 
-*/
-
+/// <summary>
+/// Keeps track of state the game is in wrt to levels and scenes 
+/// </summary>
 public class GameManager : MonoBehaviour {
+	public static GameManager s_instance;
 
 	public enum LevelState {MainMenu, POS, SailTrim, ApparentWind, LearnToTack, Docking, Navigation};
 	public LevelState thisLevelState;
 
-	public static GameManager s_instance;
+	// Game pausing event
+	public delegate void GamePause( bool toggle );
+	public static event GamePause PauseEvent;
+
+	public bool isPaused = false;
+
+	[Header("UI")]
+	public CanvasGroup pauseMenu;
 
 	void Awake() {
 		if (s_instance == null) {
@@ -28,5 +36,34 @@ public class GameManager : MonoBehaviour {
 		SceneManager.LoadScene (levelIndex);
 	}
 
+	public void PressedPause() {
+		if( PauseEvent != null ) {
+			PauseEvent( true );
+		}
+
+		isPaused = true;
+		pauseMenu.alpha = 1f;
+		pauseMenu.interactable = true;
+		pauseMenu.blocksRaycasts = true;
+	}
+
+	public void ResumeGame() {
+		isPaused = false;
+		pauseMenu.alpha = 0f;
+		pauseMenu.interactable = false;
+		pauseMenu.blocksRaycasts = false;
+
+		if( PauseEvent != null ) {
+			PauseEvent( false );
+		}
+	}
+
+	public void RestartModule() {
+		LoadLevel( (int)thisLevelState );
+	}
+
+	public void ReturnToMainMenu() {
+		LoadLevel( (int)LevelState.MainMenu );
+	}
 }
 
