6ce9c157d7b28aa4dc2059549bca43f9a1c8dd41
paleonluna <paleonluna@gmail.com>
2016-01-28 17:06:10 -0800

    progrss to docking module

--- Assets/Scripts/Misc/ExitLandingZone.cs
+++ Assets/Scripts/Misc/ExitLandingZone.cs
@@ -3,11 +3,11 @@ using System.Collections;
 
 public class ExitLandingZone : MonoBehaviour {
 
-	void OnTriggerEnter(Collider other) {
-		if (other.tag == "Player" && MOBManager.s_instance !=null) {
+	void OnCollisionEnter(Collision other) {
+		if (other.gameObject.tag == "Player" && MOBManager.s_instance !=null) {
 			MOBManager.s_instance.Fail ();
 		}
-		if (other.tag == "Player" && DockingManager.s_instance !=null) {
+		if (other.gameObject.tag == "Player" && DockingManager.s_instance !=null) {
 			DockingManager.s_instance.Fail ();
 		}	
 	}
