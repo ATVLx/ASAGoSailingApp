aad5096cfd4fd8111a96e7542d063c4d5c4967c7
paleonluna <paleonluna@gmail.com>
2016-02-01 11:53:49 -0800

    fixed several bugs in MOB and added arrows

---
+++ Assets/DestroyAfterX.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyAfterX : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine ("Death");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator Death() {
+		yield return new WaitForSeconds (3f);
+		Destroy (gameObject);
+	}
+}
