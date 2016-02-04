37135e75ce6ecf6778e7f4c2132dcedff7a66d34
paleonluna <paleonluna@gmail.com>
2016-02-04 10:27:40 -0800

    changes to POS prefabs and scene

---
+++ Assets/PlayBeep.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayBeep : MonoBehaviour {
+
+	public void Beep () {
+		if (SoundtrackManager.s_instance != null)
+			SoundtrackManager.s_instance.PlayAudioSource (SoundtrackManager.s_instance.beep);
+	}
+}
