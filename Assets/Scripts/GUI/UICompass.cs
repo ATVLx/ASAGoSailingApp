c339a730684c7e9dcf10b7254373c8f68b4f9da0
Eric Espinoza <ericespinoza@outlook.com>
2016-02-05 18:15:41 -0800

    Programmed UICompass.

---
+++ Assets/Scripts/GUI/UICompass.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UICompass : MonoBehaviour {
+
+	/// <summary>
+	/// The childed compass indicator.
+	/// </summary>
+	public RectTransform childedCompassIndicator;
+
+	public Transform boatTransform;
+
+	void Start () {
+		if( childedCompassIndicator == null ) {
+			childedCompassIndicator = transform.GetChild( 0 ).GetComponent<RectTransform>();
+		}
+
+		if( boatTransform == null ) {
+			Debug.LogWarning( gameObject.name +"'s "+ this.name +" was missing a reference to player's boat transform. Looking for boat by searching for \"PLayer\" tag instead." );
+			boatTransform = GameObject.FindGameObjectWithTag( "Player" ).transform;
+		}
+	}
+
+	void LateUpdate () {
+		Quaternion newRot = Quaternion.Euler( 0f, 0f, boatTransform.rotation.eulerAngles.y );
+		childedCompassIndicator.rotation = newRot;
+	}
+}
