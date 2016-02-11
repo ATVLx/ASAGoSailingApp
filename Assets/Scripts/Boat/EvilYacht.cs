50a9f0edd214d280aa597b36b2949dcdfb588637
paleonluna <paleonluna@gmail.com>
2016-02-10 17:33:26 -0800

    changes to navmangarer and nav boat control

--- Assets/Scripts/Boat/EvilYacht.cs
+++ Assets/Scripts/Boat/EvilYacht.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class EvilYacht : MonoBehaviour {
 	public bool isMoving;
+	public float speed = 100000f;
 	// Use this for initialization
 	void Start () {
 	
@@ -11,7 +12,7 @@ public class EvilYacht : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 		if (isMoving) {
-			GetComponent<Rigidbody> ().AddForce (transform.forward * 100000f);
+			GetComponent<Rigidbody> ().AddForce (transform.forward * speed);
 		}
 	}
 
