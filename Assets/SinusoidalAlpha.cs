fec98684efc8f0bff3fc823e2fb82cdc809aa435
Eric Espinoza <ericespinoza@outlook.com>
2016-02-11 18:55:33 -0800

    Updated loading screen art.

--- Assets/SinusoidalAlpha.cs
+++ Assets/SinusoidalAlpha.cs
@@ -6,12 +6,6 @@ public class SinusoidalAlpha : MonoBehaviour {
 
 	float opac = 0;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
 	void Update () {
 		opac += Time.deltaTime;
 		GetComponent<Text>().color = new Color(1,1,1,Mathf.Sin(opac));
