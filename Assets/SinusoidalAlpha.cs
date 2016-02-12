4bbc3df5f821d99cabb9939cce5cdd21b8266c06
paleonluna <paleonluna@gmail.com>
2016-02-11 18:28:51 -0800

    added start text and mode

---
+++ Assets/SinusoidalAlpha.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SinusoidalAlpha : MonoBehaviour {
+
+	float opac = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		opac += Time.deltaTime;
+		GetComponent<Text>().color = new Color(1,1,1,Mathf.Sin(opac));
+	}
+}
