72d751cc3138929335b7ea1f72d0e796faea98d2
Eric Espinoza <ericespinoza@outlook.com>
2016-02-19 16:09:31 -0800

    Going back to the main menu from a module makes the module menu go to the last played module.

--- Assets/Scripts/Managers/MainMenuManager.cs
+++ Assets/Scripts/Managers/MainMenuManager.cs
@@ -3,6 +3,61 @@ using System.Collections;
 
 public class MainMenuManager : MonoBehaviour {
 
+	public GameObject startPanel;
+	public GameObject modulesMenu;
+
+	void OnLevelWasLoaded() {
+		if (GameManager.s_instance.isReturningFromModule) {
+			GameManager.s_instance.isReturningFromModule = false;
+
+			Animator modulesMenuAnimator = modulesMenu.GetComponent<Animator>();
+			modulesMenuAnimator.SetTrigger ("menuTrigger");
+
+			InstructionsPanel modulesMenuInstructionsPanel = modulesMenu.GetComponent<InstructionsPanel> ();
+
+			/* Order of levels as slides: 
+			 * 0 POS 
+			 * 1 Apparent Wind
+			 * 2 Trim
+			 * 3 Tack
+			 * 4 RoW
+			 * 5 Dock
+			 * 6 MOB
+			 * 7 Nav */
+			int goToPanelIndex = 0;
+			switch( GameManager.s_instance.lastLevelState ) {
+			case GameManager.LevelState.POS:
+				goToPanelIndex = 0;
+				break;
+			case GameManager.LevelState.SailTrim:
+				goToPanelIndex = 2;
+				break;
+			case GameManager.LevelState.ApparentWind:
+				goToPanelIndex = 1;
+				break;
+			case GameManager.LevelState.LearnToTack:
+				goToPanelIndex = 3;
+				break;
+			case GameManager.LevelState.Docking:
+				goToPanelIndex = 5;
+				break;
+			case GameManager.LevelState.Navigation:
+				goToPanelIndex = 7;
+				break;
+			case GameManager.LevelState.ManOverboard:
+				goToPanelIndex = 6;
+				break;
+			case GameManager.LevelState.RightOfWay:
+				goToPanelIndex = 4;
+				break;
+			}
+			GameObject goToPanel = modulesMenu.transform.GetChild(0).GetChild( goToPanelIndex ).gameObject;
+			modulesMenuInstructionsPanel.GoToPanel (goToPanel);
+
+			startPanel.SetActive (false);
+		}
+	}
+
 	#region LoadLevel methods
 	public void LoadPOSModule() {
 		SoundtrackManager.s_instance.PlayAudioSource (SoundtrackManager.s_instance.beep);
