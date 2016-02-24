9277543a2b6abdaf3d8c3aa655690b9a1f7526f1
Eric Espinoza <ericespinoza@outlook.com>
2016-02-24 15:12:59 -0800

    Changed main menu so that logo appears on choose your module's position and choose your module strobes. Moved some scripts into scripts folder.

---
+++ Assets/Scripts/GUI/ChooseYourModuleStrobe.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ChooseYourModuleStrobe : MonoBehaviour {
+
+	float strobeSpeed = 1f;
+	float maxAlpha = 1f;
+	float minAlpha = 0.5f;
+	float opacity = 0;
+
+	void Update () {
+		opacity += Time.deltaTime;
+		GetComponent<Text>().color = new Color(1,1,1, ( minAlpha + Mathf.PingPong( Time.time*strobeSpeed, maxAlpha-minAlpha ) ));
+	}
+}
